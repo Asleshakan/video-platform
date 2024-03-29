26b324f5bf6e4fd10fae7a446a5a60070e8ec141
Rudy Ayitinya Sulley <78674026+ayitinya@users.noreply.github.com>
2024-03-29 08:54:26 +0000

    Add account creation test case

--- apps/server/accounts/tests.py
+++ apps/server/accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class AccountsTest(APITestCase):
+    def testAccountCreation(self):
+        data = {
+            "username": "test",
+            "email": "test@gmail.com"
+        }
+        response = self.client.post("/api/users/", data, format="json")
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        
