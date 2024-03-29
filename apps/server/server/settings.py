6cd3bbef25ab52a6ec83ebc1775665330e204bec
Rudy Ayitinya Sulley <78674026+ayitinya@users.noreply.github.com>
2024-03-29 09:19:37 +0000

    Add videos app to INSTALLED_APPS

--- apps/server/server/settings.py
+++ apps/server/server/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "djoser",
+    "accounts.apps.AccountsConfig",
+    "videos.apps.VideosConfig",
 ]
 
 MIDDLEWARE = [
