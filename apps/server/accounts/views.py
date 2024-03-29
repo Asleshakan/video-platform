dca48785435d646c239ec17c13c74e728df24770
Rudy Ayitinya Sulley <78674026+ayitinya@users.noreply.github.com>
2024-03-29 11:30:24 +0000

    Refactor UserSerializer to UserModelSerializer in accounts serializers and views
    UserModel was conflicting with a similarly named serializer from djoser

--- apps/server/accounts/views.py
+++ apps/server/accounts/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from .serializers import UserSerializer
+from .serializers import UserModelSerializer
+
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserModelSerializer
