bfde836d74b3e62f0f8388c38559eaf14d5bb2b7
Rudy Ayitinya Sulley <78674026+ayitinya@users.noreply.github.com>
2024-03-31 01:47:12 +0000

    Add Suspense component to App.vue

--- apps/spa/src/App.vue
+++ apps/spa/src/App.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <RouterView />
+  <Suspense>
+    <RouterView />
+  </Suspense>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
